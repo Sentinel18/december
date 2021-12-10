5bad02ff9ca9de90fa0712f090a1cd408f383536
Bakó Erik <rikerik7@gmail.com>
2021-12-10 11:34:58 +0100

    ora13

---
+++ ora13/koratmero.py
@@ -0,0 +1,17 @@
+import easygui
+import math
+
+try:
+    diameter_str = easygui.enterbox("A kör átmérője cm-ben",title="Adatbekérés")
+    diameter = float(diameter_str)
+    if diameter> 0:
+        radius = diameter/2
+        kerulet = 2*radius * math.pi
+        terulet = radius**2*math.pi
+        easygui.msgbox("A {} cm átmérőjű kör kerülete {} cm, területe pedig: {} cm^2".format(diameter,kerulet,terulet))
+
+    else:
+        easygui.msgbox("Nem megfelelő az érték",title="hiba")
+except:
+    easygui.msgbox("Nem megflelő az érték:", title="Hiba")
+
