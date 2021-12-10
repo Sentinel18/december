441a6acf444e64657b5da9006985fa7711bf0e3c
Bakó Erik <rikerik7@gmail.com>
2021-12-10 10:50:11 +0100

    ora13

--- ora13/scatter.py
+++ ora13/scatter.py
@@ -4,3 +4,7 @@ import numpy as np
 number_of_points = 100
 x = np.random.randint(0,500,number_of_points)
 y = x + 30 * np.random.rnd(number_of_points)
+size = np.random.randint(0,200,number_of_points)
+color = np.random.randint(0,10,number_of_points)
+plt.scatter(x,y, s=size)
+plt.show()
