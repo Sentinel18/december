d563cf0877053db745c83481cab9bcadaf97166c
Bakó Erik <rikerik7@gmail.com>
2021-12-10 11:10:11 +0100

    ora13

--- ora13/gui.py
+++ ora13/gui.py
@@ -1,3 +1,13 @@
 from easygui import *
+"""
+user_response = msgbox("Hello There!", title = "General Kenobi", ok_button="I have the high ground!",image="python_logo.png")
 
-msgbox("xd")
+print(user_response)
+
+flavor = buttonbox("Melyik a kedvenc fagylaltod?", title = "fagylalt", choices=["csoki","eper", "torta"],
+                    images="python_logo.png", default_choice="csoki")
+"""
+
+flavor = choicebox("Melyik a kedvenc fagylaltod?", title = "fagylalt", choices=["csoki","eper", "torta"], preselect=1)
+
+print(flavor)
